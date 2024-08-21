b1d0e0aa5851bac95dc91262a684ebcb9ea2c97c
ft4bhi <psabhiram100@gmail.com>
2024-08-21 21:52:03 +0530

    checkout abhiram

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { config } from 'dotenv';
 
-config({ path: '.env.development.local' }); // or .env
+config({ path: '.env.local' }); // or .env
 
 export const db = drizzle(sql);
 
